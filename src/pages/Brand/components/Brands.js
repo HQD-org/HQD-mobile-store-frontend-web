2ba39fd4a31b8d04b3148875549d69ebe913005f
peunruly <quynhdothy@gmail.com>
2021-10-20 12:53:25 +0700

    [Quynh: update] Search by name, filter by status, update-delete brand

--- src/pages/Brand/components/Brands.js
+++ src/pages/Brand/components/Brands.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../../common/css/Brand.Style.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import logoiPhone from "../../../common/images/iPhone-logo.jpg";
 import logoOppo from "../../../common/images/oppo-logo.jpg";
+import Upload from "../components/Upload";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import UpdateBtn from "../../../common/images/add-button.png";
 import logoSamsung from "../../../common/images/samsung-logo.png";
 
-const Brands = () => {
+const Brands = (props) => {
+  const { buttonLabel, className } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
   return (
     <div class="row row-cols-1 row-cols-md-4 g-4" style={{ marginTop: "15px" }}>
       <div class="col">
-        <div class="card h-100 card-brand">
+        <div class="card h-100 card-brand" onClick={toggle}>
+          {buttonLabel}
           <div className="logo-brand">
             <img src={logoSamsung} class="card-img-top img-logo" alt="..." />
           </div>
@@ -24,7 +33,8 @@ const Brands = () => {
         </div>
       </div>
       <div class="col">
-        <div class="card h-100 card-brand">
+        <div class="card h-100 card-brand" onClick={toggle}>
+          {buttonLabel}
           <div className="logo-brand">
             <img src={logoiPhone} class="card-img-top img-logo" alt="..." />
           </div>
@@ -39,7 +49,8 @@ const Brands = () => {
         </div>
       </div>
       <div class="col">
-        <div class="card h-100 card-brand">
+        <div class="card h-100 card-brand" onClick={toggle}>
+          {buttonLabel}
           <div className="logo-brand">
             <img src={logoOppo} class="card-img-top img-logo" alt="..." />
           </div>
@@ -53,6 +64,99 @@ const Brands = () => {
           </div>
         </div>
       </div>
+      <div class="col">
+        <div class="card h-100 card-brand" onClick={toggle}>
+          {buttonLabel}
+          <div className="logo-brand">
+            <img src={logoOppo} class="card-img-top img-logo" alt="..." />
+          </div>
+
+          <div class="card-body">
+            <h5 class="card-title">OPPO</h5>
+            <p class="card-text">Giới thiệu sương sương về thương hiệu này</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Ngưng kinh doanh</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 card-brand" onClick={toggle}>
+          {buttonLabel}
+          <div className="logo-brand">
+            <img src={logoOppo} class="card-img-top img-logo" alt="..." />
+          </div>
+
+          <div class="card-body">
+            <h5 class="card-title">OPPO</h5>
+            <p class="card-text">Giới thiệu sương sương về thương hiệu này</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Ngưng kinh doanh</small>
+          </div>
+        </div>
+      </div>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader className="close-x" toggle={toggle}>
+          <img
+            src={UpdateBtn}
+            alt=""
+            style={{ width: "30px", marginRight: "5px" }}
+          />
+          Update Brand
+        </ModalHeader>
+        <ModalBody>
+          <form>
+            <div className="container">
+              <div class="row mb-3">
+                <label for="input-name" class="col-sm-4 col-form-label">
+                  Tên thương hiệu
+                </label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" id="input-name" />
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label for="input-name" class="col-sm-4 col-form-label">
+                  Trạng thái
+                </label>
+                <div className="col-sm-8">
+                  <select
+                    class="form-select"
+                    aria-label="Default select example"
+                  >
+                    <option selected disabled>
+                      Choose..
+                    </option>
+                    <option value="1">Hoạt động</option>
+                    <option value="2">Ngưng kinh doanh</option>
+                  </select>
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label for="input-introduce" class="col-sm-4 form-label">
+                  Giới thiệu
+                </label>
+                <div className="col-12">
+                  <textarea
+                    class="form-control"
+                    id="input-introduce"
+                    rows="4"
+                  ></textarea>
+                </div>
+              </div>
+              <Upload />
+            </div>
+          </form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary">Submit</Button>{" "}
+          <Button color="danger">Delete</Button>
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 };
--- src/pages/Brand/components/HeaderBrand.js
+++ src/pages/Brand/components/HeaderBrand.js
@@ -4,6 +4,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import Upload from "../components/Upload";
 import AddBtn from "../../../common/images/add-button.png";
+import { FormGroup, Input } from "reactstrap";
 
 const HeaderBrand = (props) => {
   const { buttonLabel, className } = props;
@@ -12,7 +13,7 @@ const HeaderBrand = (props) => {
 
   const toggle = () => setModal(!modal);
   return (
-    <div className="row" style={{ marginTop: "50px" }}>
+    <div className="row" style={{ marginTop: "50px", alignItems: "baseline" }}>
       <div className="col">
         <div style={{ display: "inline-flex" }}>
           <i class="fab fa-react icon-admin icon-brandHeader"></i>
@@ -21,73 +22,107 @@ const HeaderBrand = (props) => {
 
         <p className="choosenHQD"> Choosing HQD mobile is a good ideal</p>
       </div>
+
+      <div className="col-6">
+        {" "}
+        <form className="form-filter-brand">
+          <div className="col-4">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+          </div>
+          <div className="col-3">
+            {" "}
+            <FormGroup>
+              <Input type="select" name="select" id="filter-status">
+                <option selected disabled>
+                  Filter by Status
+                </option>
+                <option>Tất cả</option>
+                <option>Hoạt động</option>
+                <option>Ngưng kinh doanh</option>
+              </Input>
+            </FormGroup>
+          </div>
+          <div className="col-4">
+            <button type="button" className="btn-addBrand">
+              <span>Search</span>
+            </button>
+          </div>
+        </form>
+      </div>
+
       <div className="col-2">
         <button type="button" className="btn-addBrand" onClick={toggle}>
           {buttonLabel}
           <i class="bi bi-plus-square-fill icon-brand"></i>
           <span>Add Brand</span>
         </button>
-        <Modal isOpen={modal} toggle={toggle} className={className}>
-          <ModalHeader className="close-x" toggle={toggle}>
-            <img
-              src={AddBtn}
-              alt=""
-              style={{ width: "30px", marginRight: "5px" }}
-            />
-            Add Brand
-          </ModalHeader>
-          <ModalBody>
-            <form>
-              <div className="container">
-                <div class="row mb-3">
-                  <label for="input-name" class="col-sm-4 col-form-label">
-                    Tên thương hiệu
-                  </label>
-                  <div class="col-sm-8">
-                    <input type="text" class="form-control" id="input-name" />
-                  </div>
-                </div>
-                <div class="row mb-3">
-                  <label for="input-name" class="col-sm-4 col-form-label">
-                    Trạng thái
-                  </label>
-                  <div className="col-sm-8">
-                    <select
-                      class="form-select"
-                      aria-label="Default select example"
-                    >
-                      <option selected disabled>
-                        Choose..
-                      </option>
-                      <option value="1">Hoạt động</option>
-                      <option value="2">Ngưng kinh doanh</option>
-                    </select>
-                  </div>
-                </div>
-                <div class="row mb-3">
-                  <label for="input-introduce" class="col-sm-4 form-label">
-                    Giới thiệu
-                  </label>
-                  <div className="col-12">
-                    <textarea
-                      class="form-control"
-                      id="input-introduce"
-                      rows="4"
-                    ></textarea>
-                  </div>
-                </div>
-                <Upload />
-              </div>
-            </form>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary">Submit</Button>{" "}
-            <Button color="secondary" onClick={toggle}>
-              Cancel
-            </Button>
-          </ModalFooter>
-        </Modal>
       </div>
+
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader className="close-x" toggle={toggle}>
+          <img
+            src={AddBtn}
+            alt=""
+            style={{ width: "30px", marginRight: "5px" }}
+          />
+          Add Brand
+        </ModalHeader>
+        <ModalBody>
+          <form>
+            <div className="container">
+              <div class="row mb-3">
+                <label for="input-name" class="col-sm-4 col-form-label">
+                  Tên thương hiệu
+                </label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" id="input-name" />
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label for="input-name" class="col-sm-4 col-form-label">
+                  Trạng thái
+                </label>
+                <div className="col-sm-8">
+                  <select
+                    class="form-select"
+                    aria-label="Default select example"
+                  >
+                    <option selected disabled>
+                      Choose..
+                    </option>
+                    <option value="1">Hoạt động</option>
+                    <option value="2">Ngưng kinh doanh</option>
+                  </select>
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label for="input-introduce" class="col-sm-4 form-label">
+                  Giới thiệu
+                </label>
+                <div className="col-12">
+                  <textarea
+                    class="form-control"
+                    id="input-introduce"
+                    rows="4"
+                  ></textarea>
+                </div>
+              </div>
+              <Upload />
+            </div>
+          </form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary">Submit</Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 };

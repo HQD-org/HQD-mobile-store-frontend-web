0d4b704b9ed96d65545c16941031727d460a9ff9
leehari-ute <nhai30928@gmail.com>
2021-11-21 00:00:56 +0700

    [Hai: update] filter MobileModel with timeDebut and operations system

--- src/common/constants/ListSelect.js
+++ src/common/constants/ListSelect.js
@@ -1,3 +1,9 @@
+const osList = [
+  { _id: "all", name: "Tất cả" },
+  { _id: "android", name: "Android" },
+  { _id: "ios", name: "IOS" },
+];
+
 const statusBrand = [
   {
     _id: "active",
@@ -10,6 +16,13 @@ const statusBrand = [
 ];
 const statusModel = [...statusBrand, { _id: "out of stock", name: "Hết hàng" }];
 
-// const color =
+const timeDebutList = [
+  { _id: "all", name: "Tất cả" },
+  { _id: "2018", name: "2018" },
+  { _id: "2019", name: "2019" },
+  { _id: "2020", name: "2020" },
+  { _id: "2021", name: "2021" },
+  // { _id: "2022", name: "2022" },
+];
 
-export { statusBrand, statusModel };
+export { osList, statusBrand, statusModel, timeDebutList };

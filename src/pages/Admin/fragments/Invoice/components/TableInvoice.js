647f47706072cf160b7f01599b61df8a2f2ede2e
leehari-ute <nhai30928@gmail.com>
2021-12-10 22:29:39 +0700

    [Hai: new] load invoice fragment

--- src/pages/Admin/fragments/Invoice/components/TableInvoice.js
+++ src/pages/Admin/fragments/Invoice/components/TableInvoice.js
@@ -1,13 +1,32 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { numberWithCommas } from "../../../../../common/utils/helper";
+import { changeStatusOrderAction } from "../../../../../redux/actions/Order/orderAction";
 
 const TableInvoice = (props) => {
-  const { setModal, modal, type } = props;
+  const dispatch = useDispatch();
+  const { setModal, nextStatus, getInvoiceByStatus, setCurrentItem } = props;
   const invoices = useSelector((state) => state.order.list);
-  const toggle = () => {
-    setModal(!modal);
+  const pagination = useSelector((state) => state.order.pagination);
+  const toggle = (index) => {
+    setModal(true);
+    setCurrentItem(index);
   };
+
+  const changeStatusOrder = async (id) => {
+    const res = await dispatch(
+      changeStatusOrderAction({ idOrder: id, status: nextStatus })
+    );
+    if (res) getInvoiceByStatus(pagination.page, pagination.itemPerPage);
+  };
+
+  const cancelOrder = async (id) => {
+    const res = await dispatch(
+      changeStatusOrderAction({ idOrder: id, status: "cancel" })
+    );
+    if (res) getInvoiceByStatus(pagination.page, pagination.itemPerPage);
+  };
+
   return (
     <table className="table-invoice">
       <thead>
@@ -25,25 +44,33 @@ const TableInvoice = (props) => {
         {invoices.map((item, index) => {
           const receiveInfo = item.receiveInfo;
           return (
-            <tr onClick={toggle} key={item._id}>
-              <td>{index + 1}</td>
-              <td>{item._id}</td>
-              <td>{receiveInfo.receiver}</td>
-              <td>{receiveInfo.phone}</td>
-              <td>{numberWithCommas(item.totalPrice)} ₫</td>
-              <td>{item.createdAt}</td>
+            <tr key={item._id}>
+              <td onClick={() => toggle(index)}>{index + 1}</td>
+              <td onClick={() => toggle(index)}>{item._id}</td>
+              <td onClick={() => toggle(index)}>{receiveInfo.receiver}</td>
+              <td onClick={() => toggle(index)}>{receiveInfo.phone}</td>
+              <td onClick={() => toggle(index)}>
+                {numberWithCommas(item.totalPrice)} ₫
+              </td>
+              <td onClick={() => toggle(index)}>{item.createdAt}</td>
               <td style={{ textAlign: "center" }}>
                 {item.status === "cancel" || item.status === "delivered" ? (
                   <> </>
                 ) : (
-                  <button type="submit" className="btnAccept">
+                  <button
+                    onClick={() => changeStatusOrder(item._id)}
+                    className="btnAccept"
+                  >
                     Accept
                   </button>
                 )}
               </td>
               <td style={{ textAlign: "center" }}>
                 {item.status === "wait" ? (
-                  <button type="submit" className="btnCancel">
+                  <button
+                    onClick={() => cancelOrder(item._id)}
+                    className="btnCancel"
+                  >
                     Cancel
                   </button>
                 ) : (
--- src/pages/Admin/fragments/Invoice/index.js
+++ src/pages/Admin/fragments/Invoice/index.js
@@ -4,70 +4,106 @@ import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getByStatusAndBranchAction } from "../../../../redux/actions/Order/orderAction";
 import InvoiceHeader from "./components/InvoiceHeader";
+import InvoiceInformation from "./components/InvoiceInformation";
 import TableInvoice from "./components/TableInvoice";
 
 const InvoiceFragment = React.memo(() => {
   const dispatch = useDispatch();
   const branch = useSelector((state) => state.auth.branch);
-  const [value, setValue] = useState("1");
+  const firstInitRef = useRef(null);
+  const [status, setStatus] = useState("wait");
+  const [modal, setModal] = useState(false);
+  const [currentItem, setCurrentItem] = useState(-1);
+  const pagination = useSelector((state) => state.order.pagination);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  const statusList = [
+    {
+      value: "wait",
+      next: "confirmed",
+    },
+    {
+      value: "confirmed",
+      next: "delivering",
+    },
+    {
+      value: "delivering",
+      next: "delivered",
+    },
+    {
+      value: "delivered",
+      next: "",
+    },
+    {
+      value: "cancel",
+      next: "",
+    },
+  ];
 
-  const getInvoiceByStatus = async (page, itemPerPage, status) => {
+  const getInvoiceByStatus = async (page, itemPerPage) => {
     const query = {
       page,
       itemPerPage,
       status,
       idBranch: branch._id,
     };
-    console.log("log at --> invoice index --> query: ", query);
     await dispatch(getByStatusAndBranchAction(query));
   };
 
+  const handleChange = (event, newStatus) => {
+    setStatus(newStatus);
+  };
+
   useEffect(() => {
-    getInvoiceByStatus(1, 16, "wait");
+    getInvoiceByStatus(1, 16);
   }, []);
 
+  useEffect(() => {
+    if (!firstInitRef.current) {
+      firstInitRef.current = true;
+      return;
+    }
+    getInvoiceByStatus(pagination.page, pagination.itemPerPage);
+  }, [status]);
+
   return (
-    <div>
+    <>
       <InvoiceHeader />
       <div className="row container-fluid">
         <Box sx={{ width: "100%", typography: "body1" }}>
-          <TabContext value={value}>
+          <TabContext value={status}>
             <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
               <TabList onChange={handleChange}>
-                <Tab label="Chờ xác nhận" value="1" />
-                <Tab label="Chờ lấy hàng" value="2" />
-                <Tab label="Đang giao hàng" value="3" />
-                <Tab label="Giao thành công" value="4" />
-                <Tab label="Đã hủy" value="5" />
+                <Tab label="Chờ xác nhận" value="wait" />
+                <Tab label="Chờ lấy hàng" value="confirmed" />
+                <Tab label="Đang giao hàng" value="delivering" />
+                <Tab label="Giao thành công" value="delivered" />
+                <Tab label="Đã hủy" value="cancel" />
               </TabList>
             </Box>
-            <TabPanel value="1">
-              <TableInvoice />
-            </TabPanel>
-            <TabPanel value="2">
-              <TableInvoice />
-            </TabPanel>
-            <TabPanel value="3">
-              <TableInvoice />
-            </TabPanel>
-            <TabPanel value="4">
-              <TableInvoice />
-            </TabPanel>
-            <TabPanel value="5">
-              <TableInvoice />
-            </TabPanel>
+            {statusList.map((item) => (
+              <TabPanel value={item.value}>
+                <TableInvoice
+                  setCurrentItem={setCurrentItem}
+                  setModal={setModal}
+                  nextStatus={item.next}
+                  getInvoiceByStatus={getInvoiceByStatus}
+                />
+              </TabPanel>
+            ))}
           </TabContext>
         </Box>
       </div>
-    </div>
+
+      <InvoiceInformation
+        setModal={setModal}
+        modal={modal}
+        currentItem={currentItem}
+      />
+    </>
   );
 });
 

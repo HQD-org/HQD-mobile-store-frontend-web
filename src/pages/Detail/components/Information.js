b4a2ea7a5ff6f9b2a34923d67e1f60fdbd678a32
peunruly <quynhdothy@gmail.com>
2021-12-27 00:34:59 +0700

    [Quynh: new] list coupon at detail page + fix something

--- src/pages/Detail/components/Information.js
+++ src/pages/Detail/components/Information.js
@@ -7,17 +7,28 @@ import React, { useEffect, useState } from "react";
 import { BsBox } from "react-icons/bs";
 import { FcApproval, FcClock, FcTwoSmartphones } from "react-icons/fc";
 import { MdSecurity, MdSync } from "react-icons/md";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllCouponAction } from "../../../redux/actions/Coupon/couponAction";
+import { formatDate, numberWithCommas } from "../../../common/utils/helper";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 const Information = () => {
   const [value, setValue] = useState("1");
   const product = useSelector((state) => state.product.productDetail);
+  const list = useSelector((state) => state.coupon.list);
   const [brand, setBrand] = useState({});
   const [model, setModel] = useState({});
+  const dispatch = useDispatch();
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  useEffect(() => {
+    const getAllCoupon = async () => {
+      await dispatch(getAllCouponAction(false));
+    };
 
+    getAllCoupon();
+  }, []);
   useEffect(() => {
     if (product.brand) setBrand(product.brand);
     if (product.model) setModel(product.model);
@@ -30,6 +41,7 @@ const Information = () => {
             <TabList onChange={handleChange} aria-label="lab API tabs example">
               <Tab label="Giới thiệu" value="1" />
               <Tab label="Chính sách" value="2" />
+              <Tab label="Khuyến mãi" value="3" />
             </TabList>
           </Box>
           <TabPanel value="1">
@@ -54,6 +66,34 @@ const Information = () => {
             <p>
               <MdSync /> 1 đổi 1 trong 30 ngày đối với sản phẩm lỗi
             </p>
+          </TabPanel>
+          <TabPanel value="3">
+            {list.map((c) => {
+              return c.status === "active" ? (
+                <div>
+                  <p>
+                    <CheckCircleIcon style={{ color: "#3FA5EF" }} />{" "}
+                    {c.description} từ ngày
+                    <span style={{ color: "red" }}>
+                      {" "}
+                      {formatDate(c.startedDate)}{" "}
+                      <span style={{ color: "black" }}>đến ngày</span>{" "}
+                      {formatDate(c.expiredDate)}{" "}
+                      <p>
+                        Mã khuyến mãi:{" "}
+                        <span style={{ color: "#3FA5EF" }}>
+                          {c.name} (Giảm tối đa{" "}
+                          {numberWithCommas(c.maxDiscount)}₫ với đơn hàng tối
+                          thiểu {numberWithCommas(c.minPriceToApply)}₫){" "}
+                        </span>
+                      </p>
+                    </span>
+                  </p>
+                </div>
+              ) : (
+                <></>
+              );
+            })}
           </TabPanel>
         </TabContext>
       </Box>
